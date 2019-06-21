ab45cf43058086a93e4f3df731c77349e36f4360
Mukesh Singh <mukeshdbit13495@gmail.com>
2019-06-21 10:10:37 +0530

    commit

---
+++ login.js
@@ -0,0 +1,14 @@
+var btn=document.getElementById('btn-login');
+btn.style.display="none";
+function myFunction(){
+    var div=document.getElementById('content');
+    
+    if(div.style.display==="block"){
+        div.style.display="none"
+        btn.style.display="block";
+    }
+    else{
+        div.style.display="block";
+        btn.style.display="none";
+    }
+}
